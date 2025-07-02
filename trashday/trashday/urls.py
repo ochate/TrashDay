9b0373b178f7ac51fc0ccbfeebd0f62f60bd57e8
ochate <gmnftmdssagk@gmail.com>
2025-07-03 00:39:09 +0900

    ルーティング設定

--- trashday/trashday/urls.py
+++ trashday/trashday/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+# プロジェクトのルーティング設定
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),    # 管理サイト用
+    path('', include('main.urls')),     # mainアプリケーションのurlsファイルに処理を引き継ぎ
 ]
